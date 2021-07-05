7727d7c0b5d057d5ccef86f3ed0265a655afabc5
Reinaldo <reinaldo.riant@kompas.com>
2021-07-05 17:19:22 +0700

    Initial Commit

---
+++ fruits-project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fruits-project
+//
+//  Created by TI Digital on 05/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fruits-project/fruits_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  fruits_projectApp.swift
+//  fruits-project
+//
+//  Created by TI Digital on 05/07/21.
+//
+
+import SwiftUI
+
+@main
+struct fruits_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
