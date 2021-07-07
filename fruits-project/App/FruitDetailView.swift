e0017617432644fd65f6ae827e03d458cddf6359
Reinaldo <reinaldo.riant@kompas.com>
2021-07-07 08:09:35 +0700

    + add header image

--- fruits-project/App/FruitDetailView.swift
+++ fruits-project/App/FruitDetailView.swift
@@ -14,7 +14,45 @@ struct FruitDetailView: View {
     
     // Mark: Body
     var body: some View {
-        Text(fruit.title)
+        NavigationView{
+            ScrollView(.vertical,showsIndicators:false){
+                VStack(alignment:.center,spacing:20){
+                    //Header
+                    
+                    FruitHeaderView(fruit: fruit)
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        
+                        //Title
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        //Headline
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        
+                        //Nutrients
+                        
+                        //Subheadline
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(fruit.gradientColors[1])
+                        //Description
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        //Link
+                    }//:Vstack
+                    .padding(.horizontal,20)
+                    .frame(maxWidth:640, alignment: .center)
+                }//: Vstack
+                .navigationBarTitle(fruit.title, displayMode: .inline)
+                .navigationBarHidden(true)
+            }//:scrollview
+            .edgesIgnoringSafeArea(.top)
+        }//:navigation
     }
 }
 
